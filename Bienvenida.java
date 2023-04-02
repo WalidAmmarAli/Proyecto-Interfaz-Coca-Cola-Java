21966133886c930da8ccda900687bcb0786b2a02
WalidAliSandovalE <ammaralialiwa@gmail.com>
2023-04-02 09:42:51 +0200

    resultado final

--- Bienvenida.java
+++ Bienvenida.java
@@ -3,8 +3,8 @@ import java.awt.event.*;
 import java.awt.*;
 
 public class Bienvenida extends JFrame implements ActionListener { // Se implementa ActionListener en vez de AWTEventListener
-  private JTextField textField1;
-  private JLabel label, label2, label3, label4;
+  private JTextField textField1 , textField2;
+  private JLabel label, label2, label3, label4, label5;
   private JButton button1;
 
   public Bienvenida(){
@@ -12,36 +12,49 @@ public class Bienvenida extends JFrame implements ActionListener { // Se impleme
     setTitle("Bienvenido");
     getContentPane().setBackground(new Color(255,0,0));
     setIconImage(new ImageIcon(getClass().getResource("images/icon.png")).getImage());
-    ImageIcon imagen = new ImageIcon("images/logo-coca.png");
+    ImageIcon imagen = (new ImageIcon( getClass().getResource("images/logo.png")));
+    
     label = new JLabel(imagen);
-    label.setBounds(25,15,300,150);
+    label.setBounds(25,2,300,140);
     add (label);
 
+
     label2 = new JLabel("Sistema Vacacional");
-    label2.setBounds(35,135,300,30);
-    label2.setFont(new Font("Andale Mono", 3, 18));
+    label2.setBounds(80,100,400,50);
+    label2.setFont(new Font("Andale Mono", 3, 20));
     label2.setForeground(new Color(255,255,255,255));
 
-    label3 = new JLabel("Ingrese su nombre");
-    label3.setBounds(45,212,200,30);
+    label3 = new JLabel("Ingrese su ID");
+    label3.setBounds(45,170,200,30);
     label3.setFont(new Font("Andale Mono", 3, 18));
     label3.setForeground(new Color(255,255,255,255));
 
-    label4 = new JLabel("Coca-Cola Company");
+    label4 = new JLabel("Coca-Cola @Company");
     label4.setBounds(85,375,300,30);
     label4.setFont(new Font("Andale Mono", 3, 18));
     label4.setForeground(new Color(255,255,255,255));
 
     textField1 = new JTextField();
-    textField1.setBounds(45,240,255,25);
+    textField1.setBounds(45,210,255,25);
     textField1.setBackground(new Color(224,224,224));
-    textField1.setFont(new Font("Andale Mono", 3, 14)); // Se corrige el uso de label4 en vez de textField1
-    textField1.setForeground(new Color(255,0,0)); // Se cambia el color del texto a rojo
+    textField1.setFont(new Font("Andale Mono", 1, 14)); // Se corrige el uso de label4 en vez de textField1
+    textField1.setForeground(new Color(0, 0, 0)); // Se cambia el color del texto a rojo
 
-    button1 = new JButton("Ingresar");
-    button1.setBounds(125,280,100,30);
-    button1.setFont(new Font("Andale Mono", 3, 14));
-    button1.setForeground(new Color(255,0,0));
+    label5 = new JLabel("Ingrese su Password");
+    label5.setBounds(45,240,200,30);
+    label5.setFont(new Font("Andale Mono", 3, 18));
+    label5.setForeground(new Color(255,255,255,255));
+
+    textField2 = new JTextField();
+    textField2.setBounds(45,280,255,25);
+    textField2.setBackground(new Color(224,224,224));
+    textField2.setFont(new Font("Andale Mono", 1, 14)); // Se corrige el uso de label4 en vez de textField1
+    textField2.setForeground(new Color(0, 0, 0)); // Se cambia el color del texto a rojo
+
+    button1 = new JButton("Loggin");
+    button1.setBounds(125,320,100,30);
+    button1.setFont(new Font("Andale Mono", 3, 16));
+    button1.setForeground(new Color(0, 0, 0));
     button1.addActionListener(this); // Se agrega el action listener al botón
     add(button1);
 
@@ -50,6 +63,8 @@ public class Bienvenida extends JFrame implements ActionListener { // Se impleme
     add(label3);
     add(label4);
     add(textField1);
+    add(label5);
+    add(textField2);
 
     // Se ajustan las dimensiones de la ventana
     setSize(350,450);
